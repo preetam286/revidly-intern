1ec8e5b43374b0e5593041a513bd3054e0eb704c
preetam286 <preetamswaraj286@gmail.com>
2020-04-23 19:41:49 +0530

    Updates

---
+++ precache-manifest.68849bef9683239614934af3258cb0ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa602c54607abb1a099643f6cc3b8a62",
+    "url": "/revidly-intern/index.html"
+  },
+  {
+    "revision": "e198be90ff13bef062b5",
+    "url": "/revidly-intern/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bbee8000e04a0283f4d2",
+    "url": "/revidly-intern/static/css/main.90d73a0c.chunk.css"
+  },
+  {
+    "revision": "e198be90ff13bef062b5",
+    "url": "/revidly-intern/static/js/2.79f08836.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/revidly-intern/static/js/2.79f08836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbee8000e04a0283f4d2",
+    "url": "/revidly-intern/static/js/main.9c87fa71.chunk.js"
+  },
+  {
+    "revision": "4af8fb426c4938a7be9c",
+    "url": "/revidly-intern/static/js/runtime-main.5bc0bdf6.js"
+  },
+  {
+    "revision": "b035f183394c4045f7a527dbecc553ab",
+    "url": "/revidly-intern/static/media/content.b035f183.jpg"
+  },
+  {
+    "revision": "418e80732678558382e0ea5f5151c342",
+    "url": "/revidly-intern/static/media/logo.418e8073.png"
+  }
+]);
